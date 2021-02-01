6d50a4e764c241c1082ac547c5a4f893b99098b0
Auxes1s <muignacio@up.edu.ph>
2021-02-01 20:03:11 +0800

    First commit

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row">
+       <div class="row">
+           <div class="col-8">
+               <img src="/storage/{{ $post->image }}" class="img-fluid">
+           </div>
+           <div class="col-4">
+               <div>
+                   <div class="d-flex align-items-center">
+                       <div class="pr-3">
+                           <img src="/storage/{{ $post->user->profile->image }}" alt="" class="rounded-circle img-fluid" style="max-width: 75px">
+                       </div>
+                       <div>
+                           <div class="font-weight-bold ">
+                               <a href="/profile/{{$post->user->id}}">
+                                   <span class="text-dark">{{ $post->user->username }} &#8226;
+                                   </span>
+                               </a>
+                               <a href ="#">Follow </a>
+                           </div>
+                       </div>
+                   </div>
+                   <hr>
+                   <div>
+                       <span class="font-weight-bold text-dark">
+                           <a href="/profile/{{$post->user->id}}">
+                               <span class="text-dark">{{ $post->user->username }}
+                               </span>
+                           </a>
+                       </span> {{ $post->caption }}
+                   </div>
+               </div>
+
+           </div>
+       </div>
+   </div>
+</div>
+@endsection
